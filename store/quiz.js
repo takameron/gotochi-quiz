fe1da85ef72abb123cd5358171e0bb14253e207a
takameron <tech@takameron.info>
2021-10-14 14:16:37 +0900

    add: added 2 quiz sets

--- store/quiz.js
+++ store/quiz.js
@@ -18,7 +18,8 @@ export const mutations = {
       text: "", // 問題に付随する文章があればそのテキスト
       photo: "", // 写真がある場合はそのパス
       choices: [], // 選択肢
-      answer: "" // 答え
+      answer: "", // 答え
+      description // 説明
     }
     */
 
@@ -33,59 +34,136 @@ export const mutations = {
     //        quizをquiz_set.quizzesに追加
     //    quiz_setをstate.listに追加( state.list.push(quiz_set) )
 
+    // ===== 福井ふるさと百景 始 =====
+    const csvHyakkei = require('../data/hyakkei.csv')
 
-    // ===== とりあえず開発時用に固定値を入れておく 始 =====
-
-    const quizSet1 = {
+    const quizsetHyakkei = {
       id: 1,
-      title: "問題セット1",
+      title: "福井ふるさと百景",
       quizzes: []
     }
 
-    const quiz1 = {}
-    quiz1.id = 1
-    quiz1.question = "〇〇の意味は？"
-    quiz1.photo = "https://res.cloudinary.com/tsukayaku/image/upload/Blog-personal/thumbnail/default.jpg"
-    quiz1.choices = ["答え1","答え2","答え3","答え4"]
-    quiz1.answer = "答え2"
+    let lines = []
+    csvHyakkei.forEach((val, i) => {
+      const line = {} // 1列の連想配列
 
-    quizSet1.quizzes.push(quiz1)
+      line.num_p = val[0].split('_')[0]
+      line.num_c = val[0].split('_')[1]
+      line.hyakkei = val[1]
+      line.point = val[2]
+      line.description = val[3]
+      line.longitude = val[4]
+      line.latitude = val[5]
+      line.hasPhoto = val[6]
+      line.img1 = val[7]
+      line.img2 = val[8]
+      line.img3 = val[9]
+      line.img4 = val[10]
+      line.img5 = val[11]
 
-    const quiz2 = {}
-    quiz2.id = 2
-    quiz2.question = "〇〇はなぜ？"
-    quiz2.photo = "https://res.cloudinary.com/tsukayaku/image/upload/v1583049405/Blog-personal/contour_extraction_of_GIMP/processed.jpg"
-    quiz2.choices = ["答え1","答え2","答え3","答え4"]
-    quiz2.answer = "答え3"
+      if (i !== 0) // 1列目はスキップ
+        lines.push(line)
+    })
 
-    quizSet1.quizzes.push(quiz2)
-    state.list.push(quizSet1)
+    // 問題生成
+    for (let i = 1; i <= 10; i++) // 10問分
+    {
+      const quiz = {}
+      quiz.id = i
+      quiz.question = "これはどこ？"
 
-    const quizSet2 = {
+      const hasPhotos = lines.filter(line => line.hasPhoto)
+      const ansIndex = Math.floor(Math.random() * hasPhotos.length) - 1
+      quiz.photo = hasPhotos[ansIndex].img1
+      quiz.answer = hasPhotos[ansIndex].hyakkei
+      quiz.description = hasPhotos[ansIndex].point & '\n' & hasPhotos[ansIndex].description
+
+      const hyakkeies = [] // 百景だけを抽出
+      lines.forEach((val) => {
+        hyakkeies.push(val.hyakkei)
+      })
+      const choices = getRandomArray(hyakkeies, 3) // 適当に3つ抽出
+      choices.push(quiz.answer) // 答えを追加
+      quiz.choices = getShuffledArray(choices) // ごちゃまぜにする
+
+      quizsetHyakkei.quizzes.push(quiz)
+    }
+
+    state.list.push(quizsetHyakkei)
+    // ===== 福井ふるさと百景 終 =====
+
+    // ===== 『若狭路ご膳』登録メニュー提供店 始 =====
+    const csvWakasaji = require('../data/wakasaji.csv')
+
+    const quizsetWakasaji = {
       id: 2,
-      title: "問題セット2",
+      title: "『若狭路ご膳』登録メニュー提供店",
       quizzes: []
     }
 
-    const quiz3 = {}
-    quiz3.id = 1
-    quiz3.question = "□□の読み方は？"
-    quiz3.photo = "https://res.cloudinary.com/tsukayaku/image/upload/Blog-personal/thumbnail/default.jpg"
-    quiz3.choices = ["答え1","答え2","答え3","答え4"]
-    quiz3.answer = "答え4"
+    lines = []
+    csvWakasaji.forEach((val, i) => {
+      const line = {} // 1列の連想配列
 
-    quizSet2.quizzes.push(quiz3)
+      line.num = val[0]
+      line.name = val[1]
+      line.zipcode = val[2]
+      line.area = val[3]
+      line.address = val[4]
+      line.business_hours = val[5]
+      line.regular_holiday = val[6]
+      line.menu1 = val[7]
+      line.price1 = val[8]
+      line.menu2 = val[9]
+      line.price2 = val[10]
+      line.menu3 = val[11]
+      line.price3 = val[12]
+      line.menu4 = val[13]
+      line.price4 = val[14]
+      line.menu5 = val[15]
+      line.price5 = val[16]
+      line.contents = val[17]
+      line.remarks = val[18]
+      line.phone_number = val[19]
+      line.number_of_seats = val[20]
+      line.parking_number = val[21]
+      line.longitude = val[22]
+      line.latitude = val[23]
+      line.img1 = val[24]
+      line.img2 = val[25]
+      line.img3 = val[26]
+      line.img4 = val[27]
 
-    const quiz4 = {}
-    quiz4.id = 2
-    quiz4.question = "〇〇はどれ？"
-    quiz4.photo = "https://res.cloudinary.com/tsukayaku/image/upload/v1583049405/Blog-personal/contour_extraction_of_GIMP/processed.jpg"
-    quiz4.choices = ["答え1","答え2","答え3","答え4"]
-    quiz4.answer = "答え1"
+      if (i !== 0) // 1列目はスキップ
+        lines.push(line)
+    })
 
-    quizSet2.quizzes.push(quiz4)
-    state.list.push(quizSet2)
-    // ===== とりあえず開発時用に固定値を入れておく 終 =====
+    // 問題生成
+    for (let i = 1; i <= 10; i++) // 10問分
+    {
+      const quiz = {}
+      quiz.id = i
+      quiz.question = "これはどのお店？"
+
+      const hasPhotos = lines.filter(line => line.img1)
+      const ansIndex = Math.floor(Math.random() * hasPhotos.length) - 1
+      quiz.photo = hasPhotos[ansIndex].img1
+      quiz.answer = hasPhotos[ansIndex].name
+      quiz.description = hasPhotos[ansIndex].contents & '\n' & hasPhotos[ansIndex].address & '\n' & '営業時間：' & hasPhotos[ansIndex].business_hours & '\n' & '定休日：' &  hasPhotos[ansIndex].regular_holiday
+
+      const names = [] // 店舗名だけを抽出
+      lines.forEach((val) => {
+        names.push(val.name)
+      })
+      const choices = getRandomArray(names, 3) // 適当に3つ抽出
+      choices.push(quiz.answer) // 答えを追加
+      quiz.choices = getShuffledArray(choices) // ごちゃまぜにする
+
+      quizsetWakasaji.quizzes.push(quiz)
+    }
+
+    state.list.push(quizsetWakasaji)
+    // ===== 『若狭路ご膳』登録メニュー提供店 終 =====
   }
 }
 
@@ -99,4 +177,38 @@ export const getters = {
   list (state) {
     return state.list
   }
+}
+
+// 任意の個数の要素をランダムに取り出す
+// 【引数】
+//   array : 配列
+//   num   : 個数
+// 【戻り値】
+//   num個の要素を含む配列
+function getRandomArray(array, num)
+{
+  const rndArray = []
+
+  for (let i = 0; i < num; i++)
+  {
+    rndArray.push(array[ Math.floor( Math.random() * array.length ) ])
+  }
+
+  return rndArray
+}
+
+// 配列ランダムに並び変える
+// ref:https://www.nxworld.net/js-array-shuffle.html
+// 【引数】
+//   array : 最小値
+// 【戻り値】
+//   並び替えた配列
+function getShuffledArray(array)
+{
+  for (let i = array.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array;
 }
